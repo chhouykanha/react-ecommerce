2ee8c49bb7f692814a1d457add93671aaa2e4868
Kanha Chhouy <chhouykanha@gmail.com>
2024-07-01 16:35:06 +0700

    init

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
 
+// eslint-disable-next-line react/prop-types
 const ProductCard = ({data}) => {
   return (
+    // eslint-disable-next-line react/prop-types
     <div key={data.id} className="space-y-4 group hover:scale-105 transition-all duration-300">
               <div className="w-full h-[200px] lg:h-[300px] bg-gray-50 overflow-hidden relative bg-transparent">
+                {/* eslint-disable-next-line react/prop-types */}
                 <Link to={`/product/${data.id}`}>
                     <img
                     className="w-full p-5 h-full object-center bg-cover group-hover:scale-110 transition-all duration-300"
+                    // eslint-disable-next-line react/prop-types
                     src={data.image}
                     alt=""
                     />
@@ -20,6 +25,7 @@ const ProductCard = ({data}) => {
               </div>
 
               <div className="flex flex-col gap-1">
+                {/* // eslint-disable-next-line react/prop-types */}
                 <p className="line-clamp-1">{data.title}</p>
                 <p>${data.price}</p>
               </div>
